3e55223b4cb963066f16cb943dfd82b5ea083643
BodruddozaRedoy <bodruddozaredoy@gmail.com>
2025-02-10 20:33:38 +0600

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,14 @@
+import "./app.scss"
+
 const App = () => {
-  return <div>Hello World</div>;
+  return <div>
+    <section>Home</section>
+    <section>Home</section>
+    <section>Home</section>
+    <section>Home</section>
+    <section>Home</section>
+    <section>Home</section>
+  </div>;
 };
 
 export default App;
